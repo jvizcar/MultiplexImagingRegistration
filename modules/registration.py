2846be32f522ed5c16cfb2cec8281f9a6f6d0427
JC <jvizcar@emory.edu>
2020-07-31 05:27:24 -0400

    rigid registration notebook

--- modules/registration.py
+++ modules/registration.py
@@ -1,13 +1,170 @@
 """
 Functions for performing different registration approaches and calculating performance metrics.
+
+List of functions:
+* register_images
+* tre_distance
+* match_keypoints
+* apply_transform
+* register_images_adv
 """
 import numpy as np
 import cv2
 from skimage import transform
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 
 # KAZE features are used as the default keypoints
 FD = cv2.KAZE_create(extended=True)
 LINEAR_TRANSFORMER = transform.SimilarityTransform()
+
+
+def drawKeyPts(im, keyp, col, th):
+    for curKey in keyp:
+        x = np.int(curKey.pt[0])
+        y = np.int(curKey.pt[1])
+        size = np.int(curKey.size)
+        cv2.circle(im, (x, y), size, col, thickness=th, lineType=8, shift=0)
+    return im
+
+
+def register_images_adv(moving_im, target_im, feature_detector, matcher, visuals=False, savepath=None):
+    """Register a moving image to a target image using OpenCV based image features and keypoint matching strategies.
+    The registration done with matched keypoints is rigid, applying both translation and rotation but no warping
+    transformations.
+
+    Paramters
+    ---------
+    moving_im : array-like
+        the image to transform when registering to target image
+    target_im : array-like
+        target image
+    feature_detector : cv2 object (feature detector)
+        the feature detector to use, must have the detectAndCompute method
+    matcher : opencv object
+        a feature matcher from opencv
+    visuals : bool (default: False)
+        if True then visuals will be displayed for each step
+    savepath : str (default: None)
+        file dir + filename to save image to
+
+    Return
+    ------
+    results : dict
+        information about the workflow
+
+    """
+    results = {}
+
+    # detect keypoints for both images
+    moving_kpts, moving_desc = feature_detector.detectAndCompute(moving_im, None)
+    target_kpts, target_desc = feature_detector.detectAndCompute(target_im, None)
+
+    # add number of keypoints detected in each image
+    results['moving-n_kpts'] = len(moving_kpts)
+    results['target-n_kpts'] = len(target_kpts)
+
+    # match keypoints between moving and target keypoints
+    matches = matcher.match(moving_desc, target_desc)
+
+    # add the number of keypoints that matched
+    results['n_matched_kpts'] = len(matches)
+
+    # subset to only the matched keypoints for each image
+    moving_matched_kpts = [moving_kpts[match.queryIdx] for match in matches]
+    target_matched_kpts = [target_kpts[match.trainIdx] for match in matches]
+
+    # convert the keypoints to a numpy array
+    moving_matched_pts = np.float32([kpt.pt for kpt in moving_matched_kpts])  # (x,y) coords
+    target_matched_pts = np.float32([kpt.pt for kpt in target_matched_kpts])  # (x,y) coords
+
+    # find homography matrix and mask to filter bad points with Ransac
+    mask = cv2.findHomography(moving_matched_pts, target_matched_pts, cv2.RANSAC, ransacReprojThreshold=10)[1]
+
+    # use the mask to keep only "good" keypoints
+    moving_filtered_kpts = [moving_matched_kpts[i] for i in np.arange(0, len(mask)) if mask[i] == [1]]
+    target_filtered_kpts = [target_matched_kpts[i] for i in np.arange(0, len(mask)) if mask[i] == [1]]
+
+    # convert the filtered keypoints to arrays
+    moving_filtered_pts = np.float32([kpt.pt for kpt in moving_filtered_kpts])
+    target_filtered_pts = np.float32([kpt.pt for kpt in target_filtered_kpts])
+
+    # add the number of filtered points
+    results['n_filtered_kpts'] = len(moving_matched_kpts)
+
+    # use the filtered pts to transform the moving image
+    transformed_im, transformed_pts = apply_transform(
+        moving_im, target_im, moving_filtered_pts, target_filtered_pts, LINEAR_TRANSFORMER
+    )
+
+    # calculate the error between the matched keypoints
+    h, w = moving_im.shape[:2]
+    results['error (raw)'] = tre_distance(moving_filtered_pts, target_filtered_pts, h, w)
+    results['error (registered)'] = tre_distance(transformed_pts, target_filtered_pts, h, w)
+
+    if visuals or savepath is not None:
+        # create the grid
+        fig = plt.figure(figsize=(20, 15))
+        gs = GridSpec(2, 3, figure=fig)
+        ax1 = fig.add_subplot(gs[0, 0])
+        ax2 = fig.add_subplot(gs[0, 1])
+        ax3 = fig.add_subplot(gs[1, 0])
+        ax4 = fig.add_subplot(gs[1, 1])
+        ax5 = fig.add_subplot(gs[1, 2])
+
+        # draw the unfiltered keypoints as red and filtered keypoitns over it as green
+        moving_im_rgb = np.stack((moving_im,) * 3, axis=-1)
+        target_im_rgb = np.stack((target_im,) * 3, axis=-1)
+
+        moving_im_kpts = drawKeyPts(moving_im_rgb, moving_kpts, (255, 0, 0), 15)
+        target_im_kpts = drawKeyPts(target_im_rgb, target_kpts, (255, 0, 0), 15)
+        moving_im_kpts = drawKeyPts(moving_im_kpts, moving_filtered_kpts, (0, 255, 0), 15)
+        target_im_kpts = drawKeyPts(target_im_kpts, target_filtered_kpts, (0, 255, 0), 15)
+
+        ax1.imshow(moving_im_kpts)
+        ax1.set_title('Moving Image with Keypoints')
+        ax2.imshow(target_im_kpts)
+        ax2.set_title('Target Image with Keypoints')
+
+        # plot the raw images and the warped image in the center
+        ax3.imshow(moving_im, cmap='gray')
+        ax3.set_title('Moving Image (Raw)')
+        ax4.imshow(transformed_im, cmap='gray')
+        ax4.set_title('Moving Image (Registered)')
+        ax5.imshow(target_im, cmap='gray')
+        ax5.set_title('Target Image (Raw)')
+
+        # adjust plots
+        ax1.set_aspect('equal')
+        ax1.set_xticklabels([])
+        ax1.set_yticklabels([])
+        ax2.set_aspect('equal')
+        ax2.set_xticklabels([])
+        ax2.set_yticklabels([])
+        ax3.set_aspect('equal')
+        ax3.set_xticklabels([])
+        ax3.set_yticklabels([])
+        ax4.set_aspect('equal')
+        ax4.set_xticklabels([])
+        ax4.set_yticklabels([])
+        ax5.set_aspect('equal')
+        ax5.set_xticklabels([])
+        ax5.set_yticklabels([])
+        fig.subplots_adjust(wspace=0.1)
+
+        plt.suptitle('Raw Error: %.5f, Registered Error: %.5f' % (results['error (raw)'],
+                                                                  results['error (registered)']))
+
+        if savepath is not None:
+            plt.savefig(savepath, bbox_inches='tight')
+
+        if visuals:
+            plt.show()
+
+        # make sure to close figure
+        plt.close(fig)
+
+    return results
 
 
 def register_images(moving_im, target_im, methods=None):
@@ -72,9 +229,9 @@ def tre_distance(moving_pts, target_pts, img_h, img_w):
 
     Parameters
     ----------
-    moving_pts : array
+    moving_pts : array-like
         coordinates of the keypoints for the moving image
-    target_pts : array
+    target_pts : array-like
         coordinates of the keypoints for the target image
     img_h : int
         height of the moving image
@@ -128,8 +285,9 @@ def match_keypoints(moving, target, feature_detector):
     dst_points = np.float32([kp2[i].pt for i in dst_match_idx])
 
     # find homography mask and filter keypoints (unsure how this works yet)
-    H, mask = cv2.findHomography(src_points, dst_points, cv2.RANSAC, ransacReprojThreshold=10)
+    _, mask = cv2.findHomography(src_points, dst_points, cv2.RANSAC, ransacReprojThreshold=10)
 
+    # NOTE!!!! This is a bug, you should not be using the matches, instead using the matched matches
     good = [matches[i] for i in np.arange(0, len(mask)) if mask[i] == [1]]
 
     filtered_src_match_idx = [m.queryIdx for m in good]
@@ -175,7 +333,7 @@ def apply_transform(moving, target, moving_pts, target_pts, transformer, output_
         transformer.estimate(target_pts, moving_pts)
         warped_img = transform.warp(moving, transformer, output_shape=output_shape_rc)
 
-        ### Restimate to warp points
+        # restimate to warp points
         transformer.estimate(moving_pts, target_pts)
         warped_pts = transformer(moving_pts)
     else:
--- modules/utils.py
+++ modules/utils.py
@@ -1,7 +1,13 @@
 """
 Utility functions specific for this project.
+
+List of functions:
+* parse_tif_dir
+* show_keypoints
+* plot_with_keypoints
 """
 import re
+import numpy as np
 
 from os.path import join
 from os import listdir
@@ -9,37 +15,79 @@ from os import listdir
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 
+# set global matplotlib parameters
+plt.rcParams['axes.titlesize'] = 16
+plt.rcParams['figure.titlesize'] = 18
+
 
 # global variables
 FILE_REGEX = re.compile('R(?P<round>\d{1,2})_.*_c(?P<channel>\d)_ORG.*.tif')
+FILE_REGEX_Q = re.compile('R(?P<round>\d{1,2}(Q|))_.*_c(?P<channel>\d)_ORG.*.tif')
 
 
-def parse_tif_dir(data_dir):
-    """Read files in a ome tiff data directory (tif image per channel), especifically for the OHSU datasets.
-    Function returns a dictionary with keys for rounds and channels parsed_dir[round#][channel#] and the values
+def normalize_image(im):
+    """Normalize an image into the range 0-255 as type np.uint8
+
+    source: https://stats.stackexchange.com/questions/70801/how-to-normalize-data-to-0-1-range
+
+    Parameters
+    ----------
+    im : array-like
+        this should be a 2D image from the ome.tiff (or similar) that have a wide range of values. Don't pass a regular
+        RGB image or an image that is already in the range of 0 to 255
+
+    Return
+    ------
+    normalized_im : array-like
+        the normalized image
+
+    """
+    pixels = im.flatten()
+
+    # scale pixels to range 0 to 1
+    normalized_im = (pixels - np.min(pixels)) / (np.max(pixels) - np.min(pixels))
+
+    # scale the pixels by 255
+    normalized_im = (normalized_im.reshape(im.shape) * 255).astype(np.uint8)
+
+    return normalized_im
+
+
+def parse_tif_dir(data_dir, quench=False):
+    """Read files in a ome tiff data directory (tif image per channel in round), specifically for the OHSU datasets.
+    Function returns a dictionary with keys for rounds and channels (i.e. parsed_dir[round#][channel#]) and the values
     being the path to the file.
 
     Parameters
     ----------
     data_dir : str
         path to directory with the tiff images
+    quench : bool (default: False)
+        if True then quench rounds will be included in key R##Q
 
     Return
     ------
     parsed_dir : dict
         dictionary containing the image files in nested format, first level keys are the round numbers followed
-        by the channel key. Quenched rounds are ignored.
+        by the channel key
 
     """
     parsed_dict = {}
 
+    file_regex = FILE_REGEX_Q if quench else FILE_REGEX
+
     for filename in listdir(data_dir):
-        m = FILE_REGEX.search(filename)
+        m = file_regex.search(filename)
 
         if m:
             m.groupdict()
 
-            _round, channel = int(m['round']), int(m['channel'])
+            try:
+                _round = int(m['round'])
+            except ValueError:
+                _round = m['round']
+
+            channel = int(m['channel'])
 
             # seed the round dict if not created
             if _round not in parsed_dict:
@@ -51,7 +99,7 @@ def parse_tif_dir(data_dir):
     return parsed_dict
 
 
-def show_keypoints(moving_im, target_im, moving_kpts, target_kpts, size=50):
+def show_keypoints(moving_im, target_im, moving_kpts, target_kpts, size=10):
     """Show the moving and taget images and corresponding images with keypoints shown as red dots. This function is
     purely for visualization purposes.
 
@@ -85,11 +133,95 @@ def show_keypoints(moving_im, target_im, moving_kpts, target_kpts, size=50):
     ax[3].set_title('Keypoints (Target Image)', fontsize=14)
 
     for xy in moving_kpts:
-        circ = Circle((xy[0], xy[1]), size, color=(1, 0, 0))
+        circ = Circle((xy.pt [0], xy.pt[1]), size, color=(1, 0, 0))
         ax[2].add_patch(circ)
 
     for xy in target_kpts:
-        circ = Circle((xy[0], xy[1]), size, color=(1, 0, 0))
+        circ = Circle((xy.pt[0], xy.pt[1]), size, color=(1, 0, 0))
         ax[3].add_patch(circ)
 
     plt.show()
+
+
+def plot_with_keypoints(im, kpts, figsize=(10, 10), title='', color=(1., 0, 0), size=10):
+    """Plot n number multiplex channel image (1 channel) with keypoints drawn on top of it.
+
+    Parameters
+    ----------
+    im : array-like
+        image
+    kpts : list
+        list of opencv keypoints
+    figsize : tuple (default: (10, 10))
+        size of the figure in matplotlib terms
+    title : str
+        title of the figure
+    color : tuple (defaultu (1., 0, 0))
+        the RGB color to use for keypoints (range from 0. to 1.)
+    size : int (default: 10)
+        size of the keypoints in matplotlib terms
+
+    """
+    fig, ax = plt.subplots(figsize=figsize)
+
+    ax.imshow(im, cmap='gray')
+
+    for xy in kpts:
+        circ = Circle((xy.pt[0], xy.pt[1]), size, color=color)
+        ax.add_patch(circ)
+
+    ax.set_title(title)
+
+    plt.show()
+
+
+def plot_two_images(moving_im, target_im, moving_kpts=None, target_kpts=None, figsize=(15, 10), title='',
+                    color=(1., 0, 0), kpt_size=10):
+    """Plot the moving and target images next to each other (horizontally) with the option to show keypoints as well.
+
+    Parameters
+    ----------
+    moving_im : array-like
+        moving image
+    target_im : array-like
+        target image
+    moving_kpts : list (default: None)
+        moving keypoints, if None then this is ignored
+    target_kpts : list (default: None)
+        moving keypoints, if None then this is ignored
+    figsize : tuple (default: (10, 10))
+        size of the figure in matplotlib terms
+    title : str
+        title of the figure
+    color : tuple (defaultu (1., 0, 0))
+        the RGB color to use for keypoints (range from 0. to 1.)
+    kpt_size : int (default: 10)
+        size of the keypoints in matplotlib terms
+
+    Return
+    ------
+    fig : matplotlib.figure
+        the figure object, can be used to save the figure locally
+
+    """
+    fig, ax = plt.subplots(ncols=2, figsize=figsize)
+    plt.subplots_adjust(top=1.15)  # adjust location of figure title in relationship wiht the subplots
+
+    ax[0].imshow(moving_im, cmap='gray')
+    ax[1].imshow(target_im, cmap='gray')
+    ax[0].set_title('Moving Image')
+    ax[1].set_title('Target Image')
+
+    if moving_kpts is not None:
+        for xy in moving_kpts:
+            circ = Circle((xy.pt[0], xy.pt[1]), kpt_size, color=color)
+            ax[0].add_patch(circ)
+
+    if target_kpts is not None:
+        for xy in target_kpts:
+            circ = Circle((xy.pt[0], xy.pt[1]), kpt_size, color=color)
+            ax[1].add_patch(circ)
+
+    plt.suptitle(title)
+    plt.show()
+    return fig
